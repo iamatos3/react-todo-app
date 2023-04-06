4cb192cb42d68df81473b5d5a1303ded726469f8
iamatos3 <matosian93@gmail.com>
2023-04-06 18:46:30 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PageTitle from './components/PageTitle';
+import AppHeader from './components/AppHeader';
+import styles from './styles/modules/app.module.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <PageTitle>TODO LIST</PageTitle>
+      <div className={styles.app__wrapper}>
+        <AppHeader />
+        <AppContent />
+      </div>
+    </div>
+  );
+}
+
+export default App;
